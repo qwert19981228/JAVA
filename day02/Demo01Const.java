ef4b335f94803be864ded51e8c3b97991d7ba47c
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-10-28 19:05:01 +0800

    Initial commit

---
+++ day02/Demo01Const.java
@@ -0,0 +1,29 @@
+public class Demo01Const{
+	public static void main(String[] args){
+		// 字符串常量
+		System.out.println("ABC");
+		System.out.println("");// 字符串两个双引号中间的内容为空
+		System.out.println("XYZ");
+
+		// 整数常量
+		System.out.println(30);
+		System.out.println(-500);
+
+		// 小数常量
+		System.out.println(3.14);
+		System.out.println(-2.5);
+
+		// 字符常量
+		System.out.println('A');
+		System.out.println('6');
+//		System.out.println(''); // 两个单引号中间必须有且仅有一个字符,没有不行
+//		System.out.println('AB'); // 两个单引号中间必须有且仅有一个字符,没有不行
+
+		// 布尔常量
+		System.out.println(true);
+		System.out.println(false);
+
+		// 空常量. 空常量不能直接用来打印输出
+//		System.out.println(null)
+	}
+}
