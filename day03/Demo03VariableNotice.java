a3e12a93b7cc755e3475727422efc5cb4d405a7c
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-10-29 11:56:01 +0800

    Create Demo03VariableNotice.java

---
+++ day03/Demo03VariableNotice.java
@@ -0,0 +1,14 @@
+public class Demo03VariableBotice {
+    public static void main(String[] args){
+        int num1 = 10; // 创建了一个新的变量,名叫num1
+//        int num1 = 20; // 又创建了另一个新的变量,名字叫num1,错误!
+        int num2 = 20;
+
+        int num3;
+        num3 = 30;
+
+        int num4; // 定义了一个变量,但是没有进行赋值
+        
+
+    }
+}
