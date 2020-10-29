9ddb890371e7f9c4925dd1d94f184ef044d219f9
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-10-29 11:55:48 +0800

    Update Demo02Variable.java

--- day02/Demo02Variable.java
+++ day02/Demo02Variable.java
@@ -25,6 +25,36 @@ public class Demo02Variable{
         byte num3 = 30; // 注意: 右侧数值的范围不能超过左侧数据类型的取值范围
         System.out.println(num3);
 
-        byte num4 = 400
+//        byte num4 = 400; // 右侧超出了byte数据范围,错误!
+
+        short num5 = 50;
+        System.out.println(num5);
+
+        long num6 = 3000000000L;
+        System.out.println(num6);
+
+        float num7 = 2.5F;
+        System.out.println(num7);
+
+        double num8 = 1.2;
+        System.out.println(num8);
+
+        char zifu1 = 'A';
+        System.out.println(zifu1);
+
+        zifu1 = '中';
+        System.out.println(zifu1);
+
+        boolean var1 = true;
+        System.out.println(var1);
+
+        var1 = false;
+        System.out.println(var1);
+
+        // 将一个变量的数据内容,赋值交给另一个变量
+        // 右侧的变量名称var1已经存在,里面装的是false布尔值
+        // 将右侧变量里面的false值,向左交给var2变量进行存储
+        boolean var2 = var1;
+        System.out.println(var2);
     }
 }
