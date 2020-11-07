3c0123ec7c0fbde56b929e605507d6688146368d
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-07 14:34:56 +0800

    Create Demo03IFElse.java

---
+++ day06/Demo03IFElse.java
@@ -0,0 +1,10 @@
+public class Demo03IFElse{
+    public static void main(String[] args) {
+        int num = 13;
+        if (num % 2 == 0){ // 如果除以二能够余数为0 , 说明是偶数
+            System.out.println("偶数");
+        } else {
+            System.out.println("奇数");
+        }
+    }
+}
