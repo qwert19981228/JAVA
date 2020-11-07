58d42edb4b0a9e96a636c94491c75899bc7ea0dc
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-07 15:52:11 +0800

    Create Demo08SwitchNotice.java

---
+++ day06/Demo08SwitchNotice.java
@@ -0,0 +1,16 @@
+public class Demo08SwitchNotice{
+    public static void main(String[] args) {
+        int num = 1;
+        switch (num){
+            case 1:
+                System.out.println("AAA");
+                break;
+//            case 1:
+//                System.out.println("BBB");
+//                break;
+            default:
+                System.out.println("CCC");
+                break;
+        }
+    }
+}
