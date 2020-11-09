fa7cb548e0384a7bcb00d587e9ca29759b7086ba
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-09 15:49:13 +0800

    Create Demo11DoWhile.java

---
+++ day07/Demo11DoWhile.java
@@ -0,0 +1,9 @@
+public class Demo11DoWhile{
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("原谅你啦!地上怪凉的 "+i);
+            i++;
+        } while (i <= 100);
+    }
+}
