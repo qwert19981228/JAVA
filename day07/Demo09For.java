2ed8798aa14b0a908512ff5f127e32b0b73152e2
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-09 14:30:53 +0800

    Create Demo09For.java

---
+++ day07/Demo09For.java
@@ -0,0 +1,8 @@
+public class Demo09For{
+    public static void main(String[] args) {
+        for (int i = 1;i <= 100;i++){
+            System.out.println("我错啦!原谅我吧"+i);
+        }
+        System.out.println("程序停止");
+    }
+}
