a09115da565b53668b710da23f674cd580f7d29c
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-10 14:47:07 +0800

    Update HelloWorld.java

--- day08/basic-code/src/cn/yjw/day08/demo01/HelloWorld.java
+++ day08/basic-code/src/cn/yjw/day08/demo01/HelloWorld.java
@@ -1,7 +1,11 @@
 package cn.yjw.day08.demo01;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
+//        System.out.println("Hello,World!!!");
+//        System.out.println("Hello,World!!!");
         System.out.println("Hello,World!!!");
     }
 }
