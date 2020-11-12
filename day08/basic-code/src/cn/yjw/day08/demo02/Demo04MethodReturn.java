9bfe6617106512eb84965673114a0e553b0d8579
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-12 15:54:29 +0800

    Update Demo04MethodReturn.java

--- day08/basic-code/src/cn/yjw/day08/demo02/Demo04MethodReturn.java
+++ day08/basic-code/src/cn/yjw/day08/demo02/Demo04MethodReturn.java
@@ -10,8 +10,22 @@ public class Demo04MethodReturn {
         // 我调用你 , 你来帮我计算一下 , 算完了之后 , 把结果告诉我的num变量
         int num = getsum(10,23);
         System.out.println("返回值是 " + num);
+        System.out.println("==============");
 
         printsum(10,20);
+        System.out.println("==============");
+
+        System.out.println(getsum(2,3)); // 正确写法
+        getsum(4,5); // 正确写法 , 但是返回值没有用到
+        System.out.println("==============");
+
+        // 对于void没有返回值的方法 , 只能单独 , 不能打印或者赋值
+//        System.out.println(printsum(2,3));
+//        System.out.println(void);
+
+//        int num2 = printsum(10,20); // 错误写法
+//        int num3 = void;
+//        void num4 = void;
     }
     // 我是一个方法 , 我负责两个数字相加
     // 我有返回值int , 谁调用我 , 我就把计算结果告诉谁
