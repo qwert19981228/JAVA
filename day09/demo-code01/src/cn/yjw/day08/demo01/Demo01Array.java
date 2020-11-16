daf609c19774ff8ca6c5d865540314e708b39379
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-16 13:23:47 +0800

    Create Demo01Array.java

---
+++ day09/demo-code01/src/cn/yjw/day08/demo01/Demo01Array.java
@@ -0,0 +1,15 @@
+package cn.yjw.day08.demo01;
+
+/*数组的概念: 是一种容器 , 可以同时存放多个数据值
+数组的特点:
+1.数组是一种引用数据类型
+2.数组当中的多个数据 , 类型必须统一
+3.数组的长度在程序运行期间不可改变
+* */
+public class Demo01Array {
+    public static void main(String[] args) {
+        int score1 = 100;
+        int score2 = 98;
+        int score3 = 99;
+    }
+}
