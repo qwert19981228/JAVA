fca999997e183a9ba7e1fe020b7568b974c1d2c2
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-17 18:08:21 +0800

    Create Demo05ArrayMax.java

---
+++ day09/demo-code01/src/cn/yjw/day08/demo03/Demo05ArrayMax.java
@@ -0,0 +1,23 @@
+package cn.yjw.day08.demo03;
+
+public class Demo05ArrayMax {
+    public static void main(String[] args) {
+        int[] array = {5,15,30,20,10000,-20,-30};
+
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max){
+                max = array[i];
+            }
+        }
+        System.out.println("最大值: "+ max);
+
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min){
+                min = array[i];
+            }
+        }
+        System.out.println("最小值: "+ min);
+    }
+}
