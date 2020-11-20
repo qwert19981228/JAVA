b541b39296f52539c1e24f5da3902c56aa5899c6
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-20 14:31:55 +0800

    Create Demo04OverloadPrint.java

---
+++ day08/basic-code/src/cn/yjw/day09/demo04/Demo04OverloadPrint.java
@@ -0,0 +1,35 @@
+package cn.yjw.day09.demo04;
+/*
+* 在调用输出语句的时候 , println方法其实就是进行了多种数据类型的重载形式*/
+public class Demo04OverloadPrint {
+    public static void main(String[] args) {
+        myPrint(2);
+    }
+    public static void myPrint(byte num){
+        System.out.println(num);
+    }
+    public static void myPrint(short num){
+        System.out.println(num);
+    }
+    public static void myPrint(int num){
+        System.out.println(num);
+    }
+    public static void myPrint(long num){
+        System.out.println(num);
+    }
+    public static void myPrint(float num){
+        System.out.println(num);
+    }
+    public static void myPrint(double num){
+        System.out.println(num);
+    }
+    public static void myPrint(char zifu){
+        System.out.println(zifu);
+    }
+    public static void myPrint(boolean iss){
+        System.out.println(iss);
+    }
+    public static void myPrint(String str){
+        System.out.println(str);
+    }
+}
