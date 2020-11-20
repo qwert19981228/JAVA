c2db8f4489fc72771c050db025df73c7f943e378
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-20 14:33:28 +0800

    Create Demo01ArrayParam.java

---
+++ day09/demo-code01/src/cn/yjw/day09/demo04/Demo01ArrayParam.java
@@ -0,0 +1,30 @@
+package cn.yjw.day09.demo04;
+
+/*
+* 数组可以作为方法的参数
+* 当调用方法的时候 , 向方法的小括号进行传参 , 传递进去的其实是数组的地址值
+* */
+
+public class Demo01ArrayParam {
+    public static void main(String[] args) {
+        int[] array = {10,20,30,40,50};
+        printArray(array);
+        System.out.println("===============");
+        printArray(array);
+    }
+
+    /*
+    * 三要素:
+    * 返回值类型: 只是进行打印而已 , 不需要计算 , 也没有结果 , 用void
+    * 方法名称:   printArray
+    * 参数列表:   必须给我数组 , 才能打印其中的元素 , int[] array
+    * */
+
+    public static void printArray(int[] array){
+        System.out.println("printArray方法收到的参数是:");
+        System.out.println(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
