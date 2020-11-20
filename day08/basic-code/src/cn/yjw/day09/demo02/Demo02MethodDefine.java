230ca6964346a7c5cf19767c7cf01a11a8bd4bb5
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-20 14:31:20 +0800

    Create Demo02MethodDefine.java

---
+++ day08/basic-code/src/cn/yjw/day09/demo02/Demo02MethodDefine.java
@@ -0,0 +1,18 @@
+package cn.yjw.day09.demo02;
+/*
+方法其实就是若干语句的功能集合:
+方法好比是一个工厂
+*/
+public class Demo02MethodDefine {
+    public static void main(String[] args) {
+        // 打印调用
+        System.out.println(sum(10,20));
+        // 赋值调用
+        int s = sum(20,30);
+        System.out.println(s);
+    }
+    public static int sum(int a,int b){
+        int result = a+b;
+        return result;
+    }
+}
