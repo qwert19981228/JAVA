d30a5e28482a5fae52186674b26a35d452aa604d
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-20 14:31:16 +0800

    Create HelloWorld.java

---
+++ day08/basic-code/src/cn/yjw/day09/demo01/HelloWorld.java
@@ -0,0 +1,10 @@
+package cn.yjw.day09.demo01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello,World!!!");
+        System.out.println("Hello,World!!!");
+        /*System.out.println("Hello,World!!!");*/
+
+    }
+}
