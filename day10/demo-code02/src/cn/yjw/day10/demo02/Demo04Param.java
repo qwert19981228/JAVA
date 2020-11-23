c438781022e511a95bbb9e9ab3de05c72b40fac8
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-23 16:18:22 +0800

    Create Demo04Param.java

---
+++ day10/demo-code02/src/cn/yjw/day10/demo02/Demo04Param.java
@@ -0,0 +1,16 @@
+package cn.yjw.day10.demo02;
+
+public class Demo04Param {
+    public static void main(String[] args) {
+        Phone one = new Phone();
+        one.brand = "魅族";
+        one.color = "梦幻独角兽";
+        one.price = 3699.0;
+        method(one); // 传递进去的参数其实就是地址值
+    }
+    public static void method(Phone param){
+        System.out.println(param.brand);
+        System.out.println(param.color);
+        System.out.println(param.price);
+    }
+}
