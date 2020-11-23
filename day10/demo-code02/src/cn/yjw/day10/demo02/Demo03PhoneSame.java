579fd73616c08dee69aac044034930ffa54c093e
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-11-23 15:56:23 +0800

    Create Demo03PhoneSame.java

---
+++ day10/demo-code02/src/cn/yjw/day10/demo02/Demo03PhoneSame.java
@@ -0,0 +1,40 @@
+package cn.yjw.day10.demo02;
+
+public class Demo03PhoneSame {
+    public static void main(String[] args) {
+        Phone phone1 = new Phone();
+        System.out.println(phone1.brand); // null
+        System.out.println(phone1.color); // null
+        System.out.println(phone1.price); // double
+        System.out.println("=========");
+
+        phone1.brand = "魅族";
+        phone1.color = "玫瑰金";
+        phone1.price = 3699.0;
+        System.out.println(phone1.price);
+        System.out.println(phone1.color);
+        System.out.println(phone1.brand);
+        System.out.println("=========");
+
+        phone1.call("乔布斯");
+        phone1.setMessage();
+
+        Phone phone2 = phone1;
+        System.out.println(phone2.brand); // null
+        System.out.println(phone2.color); // null
+        System.out.println(phone2.price); // double
+        System.out.println("=========");
+
+        phone2.brand = "华为";
+        phone2.color = "墨绿";
+        phone2.price = 4699.0;
+        System.out.println(phone2.price);
+        System.out.println(phone2.color);
+        System.out.println(phone2.brand);
+        System.out.println("=========");
+
+        phone2.call("任正非");
+        phone2.setMessage();
+
+    }
+}
