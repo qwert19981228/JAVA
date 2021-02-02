598b8f903cb43e705de4d0667402721e0004407b
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-02 09:36:40 +0800

    Update Student.java

--- day12/src/cn/yjw/day12/demo03/Student.java
+++ day12/src/cn/yjw/day12/demo03/Student.java
@@ -1,16 +1,29 @@
 package cn.yjw.day12.demo03;
 
 public class Student {
+    private int id; // 学号
     private String name; // 姓名
     private int age; // 年龄
     static String room; // 所在教室
+    private static int idCounter = 0; // 学号计数器,每当new了一个新对象对额时候,计数器++
+
 
     public Student(String name, int age) {
         this.name = name;
         this.age = age;
+        this.id = ++idCounter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public Student() {
+        this.id = ++idCounter;
     }
 
     public String getName() {
