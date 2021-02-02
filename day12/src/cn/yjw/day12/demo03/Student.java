d74a235c9bd30b209a108d7311834becaeba6a56
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-02 09:24:51 +0800

    Create Student.java

---
+++ day12/src/cn/yjw/day12/demo03/Student.java
@@ -0,0 +1,31 @@
+package cn.yjw.day12.demo03;
+
+public class Student {
+    private String name; // 姓名
+    private int age; // 年龄
+    static String room; // 所在教室
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
