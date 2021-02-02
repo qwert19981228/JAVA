8a4232536253d2329c80b5bdb3262bb9c1eea298
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-02 09:39:07 +0800

    Update Demo01StaticField.java

--- day12/src/cn/yjw/day12/demo03/Demo01StaticField.java
+++ day12/src/cn/yjw/day12/demo03/Demo01StaticField.java
@@ -5,9 +5,9 @@ package cn.yjw.day12.demo03;
 public class Demo01StaticField {
     public static void main(String[] args) {
         Student two = new Student("黄蓉",16);
-        two.room = "101教室";
-        System.out.println("姓名: " + two.getName() + " 年龄: " + two.getAge() + " 教室: " + two.room + " 学号: " + two.getId());
+        Student.room = "101教室";
+        System.out.println("姓名: " + two.getName() + " 年龄: " + two.getAge() + " 教室: " + Student.room + " 学号: " + two.getId());
         Student one = new Student("郭靖",19);
-        System.out.println("姓名: " + one.getName() + " 年龄: " + one.getAge() + " 教室: " + one.room + " 学号: " + one.getId());
+        System.out.println("姓名: " + one.getName() + " 年龄: " + one.getAge() + " 教室: " + Student.room + " 学号: " + one.getId());
     }
 }
