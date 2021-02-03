4b4918eaecacc4668f841af29c91050114bc3fc6
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-03 14:12:31 +0800

    Create Zi.java

---
+++ day13/src/cn/yjw/day13/Demo07/Zi.java
@@ -0,0 +1,10 @@
+package cn.yjw.day13.Demo07;
+
+public class Zi extends Fu{
+    public Zi(){
+        // super(); // 在调用父类无参构造方法
+        super(20); // 在调用父类重载的构造方法
+
+        System.out.println("子类构造方法");
+    }
+}
