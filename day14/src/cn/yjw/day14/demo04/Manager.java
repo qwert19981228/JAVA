1e61665a2fd8d8137e8f4231413cfb25d2021104
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-04 15:10:02 +0800

    Create Manager.java

---
+++ day14/src/cn/yjw/day14/demo04/Manager.java
@@ -0,0 +1,42 @@
+package cn.yjw.day14.demo04;
+
+import java.util.ArrayList;
+
+// 群主类
+public class Manager extends User{
+    public Manager() {
+    }
+
+    public Manager(String name, int money) {
+        super(name, money);
+    }
+
+    public ArrayList<Integer> send(int totalMoney,int count){
+        // 首先需要一个集合,用来存储若干红包的金额
+        ArrayList<Integer> redList = new ArrayList<>();
+
+        // 首先看一下群主有多少钱
+        int leftMoney = super.getMoney(); // 群主当前余额
+        if (totalMoney > leftMoney){
+            System.out.println("余额不足");
+            return redList;
+        }
+
+        // 扣钱,其实就是重新设置余额
+        super.setMoney(leftMoney - totalMoney);
+
+        // 发红包需要平均分成为count份
+        int avg = totalMoney / count;
+        int mod = totalMoney % count; // 余数,也就是零头
+
+        // 除不开的零头,包在最后一个红包当中
+        // 下面把红包一个个放到集合当中
+        for (int i = 0; i < count - 1; i++) {
+           redList.add(avg);
+        }
+        int last = avg + mod;
+        redList.add(last);
+
+        return redList;
+    }
+}
