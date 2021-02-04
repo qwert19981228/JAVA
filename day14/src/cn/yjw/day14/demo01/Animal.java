27b7b72e021cea7a6893f8ab0421d9d30a9cc3dd
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-04 12:33:49 +0800

    Update Animal.java

--- day14/src/cn/yjw/day14/demo01/Animal.java
+++ day14/src/cn/yjw/day14/demo01/Animal.java
@@ -3,6 +3,13 @@ package cn.yjw.day14.demo01;
 /*
 * 抽象方法:就是加上abstract关键字 , 然后去掉大括号, 直接分号结束
 * 抽象类:抽象方法所在的类, 必须是抽象类才行, 在class之前写上abstract即可
+*
+* 如何使用抽象类和抽象方法
+* 1.不能直接创建new抽象类对象
+* 2.必须用一个子类来继承抽象父类
+* 3.子类必须覆盖重写抽象父类当中所有的抽象方法
+* 覆盖重写:(实现)子类去掉抽象方法的abstract关键字, 然后补上方法体大括号
+* 4.创建子类对象进行调用
 * */
 public abstract class Animal {
 
@@ -11,6 +18,6 @@ public abstract class Animal {
 
     // 这是普通的成员方法
     public void normalMethod(){
-        
+
     }
 }
