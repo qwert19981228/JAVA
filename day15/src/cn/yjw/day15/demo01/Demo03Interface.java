e51f04fefcc510cc5c8611c4f3c8e49fa35559f3
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-05 11:39:37 +0800

    Create Demo03Interface.java

---
+++ day15/src/cn/yjw/day15/demo01/Demo03Interface.java
@@ -0,0 +1,8 @@
+package cn.yjw.day15.demo01;
+
+public class Demo03Interface {
+    public static void main(String[] args) {
+        MyInterfaceStaticImpl myInterfaceStatic = new MyInterfaceStaticImpl();
+        MyInterfaceStatic.methodStatic();
+    }
+}
