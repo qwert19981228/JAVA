0c2658f578221a42e3ff7a1d83abcb8bb8d9ee36
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-05 12:18:40 +0800

    Create Demo04Interface.java

---
+++ day15/src/cn/yjw/day15/demo01/Demo04Interface.java
@@ -0,0 +1,8 @@
+package cn.yjw.day15.demo01;
+
+public class Demo04Interface {
+    public static void main(String[] args) {
+        MyInterfacePrivateB.methodDefault1();
+        MyInterfacePrivateB.methodDefault2();
+    }
+}
