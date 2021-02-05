a06b98c1e9cb337a189f92da982a28ec58449373
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-05 14:31:16 +0800

    Create MyInterfaceB.java

---
+++ day15/src/cn/yjw/day15/demo02/MyInterfaceB.java
@@ -0,0 +1,21 @@
+package cn.yjw.day15.demo02;
+
+public interface MyInterfaceB {
+
+    // 错误写法! 接口不能有静态代码块
+//    static{
+//
+//    }
+
+    // 错误写法! 接口不能有构造方法
+//    public MyInterface(){
+//
+//    }
+
+    public abstract void methodB();
+    public abstract void methodAbs();
+
+    public default void methodDefault(){
+        System.out.println("BBB");
+    }
+}
