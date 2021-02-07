5044f20608d72f654653078aaafe1cd13df321a9
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-07 10:52:36 +0800

    Create Cat.java

---
+++ day16/src/cn/yjw/day16/demo03/Cat.java
@@ -0,0 +1,13 @@
+package cn.yjw.day16.demo03;
+
+public class Cat extends Animal{
+
+    @Override
+    public void eat() {
+        System.out.println("猫吃鱼");
+    }
+
+    public void catchMouse(){
+        System.out.println("猫抓老鼠");
+    }
+}
