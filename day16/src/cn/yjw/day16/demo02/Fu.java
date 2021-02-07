27cc63b0e6c40b6d98691102057d2ab36162ba39
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-07 10:35:20 +0800

    Update Fu.java

--- day16/src/cn/yjw/day16/demo02/Fu.java
+++ day16/src/cn/yjw/day16/demo02/Fu.java
@@ -7,4 +7,12 @@ public class Fu {
         System.out.println(num);
         System.out.println(this.num);
     }
+
+    public void method(){
+        System.out.println("父类方法");
+    }
+
+    public void methodFu(){
+        System.out.println("父类特有的方法");
+    }
 }
