2dcae771511036044c1de5acfc206d1491885b03
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-07 09:31:00 +0800

    Create Fu.java

---
+++ day16/src/cn/yjw/day16/demo01/Fu.java
@@ -0,0 +1,11 @@
+package cn.yjw.day16.demo01;
+
+public class Fu {
+    public void method(){
+        System.out.println("父类方法");
+    }
+
+    public void methodFu(){
+        System.out.println("父类特有方法");
+    }
+}
