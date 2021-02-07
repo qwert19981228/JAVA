abfe44cf67d71f6566a9b586c2327d5be69ed349
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-07 09:54:20 +0800

    Create Fu.java

---
+++ day16/src/cn/yjw/day16/demo02/Fu.java
@@ -0,0 +1,10 @@
+package cn.yjw.day16.demo02;
+
+public class Fu {
+    int num = 10;
+    public void show(){
+        int num = 30;
+        System.out.println(num);
+        System.out.println(this.num);
+    }
+}
