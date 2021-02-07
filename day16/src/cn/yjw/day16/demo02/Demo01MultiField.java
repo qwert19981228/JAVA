43fff26b26f532a55fd9d27ac93954c937439d78
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-07 09:54:08 +0800

    Create Demo01MultiField.java

---
+++ day16/src/cn/yjw/day16/demo02/Demo01MultiField.java
@@ -0,0 +1,10 @@
+package cn.yjw.day16.demo02;
+
+public class Demo01MultiField {
+    public static void main(String[] args) {
+        //
+        Fu obj = new Zi();
+        System.out.println(obj.num);
+        obj.show();
+    }
+}
