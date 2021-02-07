27fde2a6107440c3ae6af366c67a39c485489576
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-07 10:35:17 +0800

    Update Zi.java

--- day16/src/cn/yjw/day16/demo02/Zi.java
+++ day16/src/cn/yjw/day16/demo02/Zi.java
@@ -10,4 +10,13 @@ public class Zi extends Fu{
         System.out.println(super.num);
 
     }
+
+    @Override
+    public void method() {
+        System.out.println("子类方法");
+    }
+
+    public void methodZi(){
+        System.out.println("子类特有的方法");
+    }
 }
