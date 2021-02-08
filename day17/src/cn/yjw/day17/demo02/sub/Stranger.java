9e1fd85431c474fdb6ff4b5551b00702cc26b2fe
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-08 10:46:44 +0800

    Create Stranger.java

---
+++ day17/src/cn/yjw/day17/demo02/sub/Stranger.java
@@ -0,0 +1,9 @@
+package cn.yjw.day17.demo02.sub;
+
+import cn.yjw.day17.demo02.MyClass;
+
+public class Stranger {
+    public void methodStrange(){
+        System.out.println(new MyClass().num);
+    }
+}
