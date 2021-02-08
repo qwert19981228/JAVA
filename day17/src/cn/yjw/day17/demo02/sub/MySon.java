be77bf21aa8010e5325581ce8a61d667e1588381
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-08 10:46:42 +0800

    Create MySon.java

---
+++ day17/src/cn/yjw/day17/demo02/sub/MySon.java
@@ -0,0 +1,11 @@
+package cn.yjw.day17.demo02.sub;
+
+import cn.yjw.day17.demo02.MyClass;
+
+public class MySon extends MyClass {
+
+    public void methodSon(){
+        System.out.println(super.num);
+    }
+
+}
