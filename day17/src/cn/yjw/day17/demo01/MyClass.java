3642e0ecf95f45656590e762fc49c92e6afcc761
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-08 10:26:34 +0800

    Update MyClass.java

--- day17/src/cn/yjw/day17/demo01/MyClass.java
+++ day17/src/cn/yjw/day17/demo01/MyClass.java
@@ -1,14 +1,5 @@
 package cn.yjw.day17.demo01;
-/*
-* 当final关键字用来修饰一个类的时候, 格式:
-* public final class 类名称{
-*   // ...
-* }
-*
-* 含义:当前这个类不能有任何的子类(太监类)
-* 不能使用一个final类作为父类
-* 注意: 一个类如果是final的, 那么其中所有的成员方法都无法进行覆盖重写(因为没子类)
-* */
+
 public final class MyClass {
 
     public void method(){
