432f29355bc70d3a78a18f9ec6f2d43f7897fde6
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-08 10:35:49 +0800

    Create Person.java

---
+++ day17/src/cn/yjw/day17/demo01/Person.java
@@ -0,0 +1,22 @@
+package cn.yjw.day17.demo01;
+
+public class Person {
+
+    private final String name;
+
+    public Person() {
+        name = "关晓彤";
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+}
