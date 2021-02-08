aab230af3f019ddcc733e1bdbf2af5ccf7fe552e
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2021-02-08 08:47:53 +0800

    Update Zi.java

--- day17/src/cn/yjw/day17/demo01/Zi.java
+++ day17/src/cn/yjw/day17/demo01/Zi.java
@@ -2,7 +2,11 @@ package cn.yjw.day17.demo01;
 
 public class Zi extends Fu{
     @Override
-    public void method() {
-        System.out.println("子类覆盖重写父类的方法");
+    public void methodAbs() {
+
     }
+//    @Override
+//    public void method() {
+//        System.out.println("子类覆盖重写父类的方法");
+//    }
 }
